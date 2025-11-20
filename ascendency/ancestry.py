43245710d6cd387b532c00a5e1dfe1cb6f7aac85
MatysDC <RazoRmatys@gmail.com>
2025-11-20 11:52:20 +0100

    Initialisation des stats, des heros, des ascendances

---
+++ ascendency/ancestry.py
@@ -0,0 +1,53 @@
+class Archère:
+    def __init__(self, name: str, pv: int, defense: int, attack: int, mana: int, dodge: int):
+        self.name = name
+        self.pv = pv
+        self.defense = defense
+        self.attack = attack
+        self.mana = mana
+        self.dodge = dodge
+    
+    def affichage(self):
+        print("--- STATS DE l'ARCHERE ---")
+        print("Name : ",self.name)
+        print("PV : ",self.pv)
+        print("Defense : ",self.defense)
+        print("Attack : ",self.attack)
+        print("Mana :", self.mana,"(Un hero n'a pas de mana)")
+        print("Dodge :", self.dodge)
+
+class Guerrier:
+    def __init__(self, name: str, pv: int, defense: int, attack: int, mana: int, dodge: int):
+        self.name = name
+        self.pv = pv
+        self.defense = defense
+        self.attack = attack
+        self.mana = mana
+        self.dodge = dodge
+    
+    def affichage(self):
+        print("--- STATS DU GUERRIER ---")
+        print("Name : ",self.name)
+        print("PV : ",self.pv)
+        print("Defense : ",self.defense)
+        print("Attack : ",self.attack)
+        print("Mana :", self.mana,"(Un hero n'a pas de mana)")
+        print("Dodge :", self.dodge, "(Un hero n'a pas de dodge)")
+
+class Magicienne:
+    def __init__(self, name: str, pv: int, defense: int, attack: int, mana: int, dodge: int):
+        self.name = name
+        self.pv = pv
+        self.defense = defense
+        self.attack = attack
+        self.mana = mana
+        self.dodge = dodge
+    
+    def affichage(self):
+        print("--- STATS DU GUERRIER ---")
+        print("Name : ",self.name)
+        print("PV : ",self.pv)
+        print("Defense : ",self.defense)
+        print("Attack : ",self.attack)
+        print("Mana :", self.mana,)
+        print("Dodge :", self.dodge, "(Un hero n'a pas de dodge)")
---
+++ ascendency/hero.py
@@ -0,0 +1,21 @@
+class Hero:
+    def __init__(self, name: str = "Hero", pv: int = 1000, defense: int = 0, attack: int = 0, mana: int = 0, dodge: int = 0):
+        self.name = name
+        self.pv = pv
+        self.defense = defense
+        self.attack = attack
+        self.mana = mana
+        self.dodge = dodge
+
+#Affichage stats, input Equipement
+    def affichage(self):
+        print("--- STATS DE l'HERO ---")
+        print("Name : ",self.name)
+        print("PV : ",self.pv)
+        print("Defense : ",self.defense)
+        print("Attack : ",self.attack)
+        print("Mana :", self.mana,"(Un hero n'a pas de mana)")
+        print("Dodge :", self.dodge,"(Un hero n'a pas de dodge)")
+
+mon_hero = Hero()
+mon_hero.affichage()
