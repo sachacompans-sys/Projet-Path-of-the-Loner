53a98cfa0537dc1f247458dd8f718013ca93b47c
sachacompans-sys <sacha.compans@gmail.com>
2025-11-24 16:15:42 +0100

    j'en peux plus j'avance trql toucher pas au fichier armor.py et json

--- Gears/armors.py
+++ Gears/armors.py
@@ -1,5 +1,7 @@
 import json
 import random
+from time import sleep
+import os
 
 class Armor:
     def __init__(self, id: int=0, name: str='none_armor', rarity: str='none_rarity', defense: int = 0):
@@ -9,43 +11,73 @@ class Armor:
         self.defense = defense
 
     def dice_roll(self, dice_value:int=0):
-        print(" ")
+        print()
         input("Lancer le dé, afin de savoir la rareté de votre armure ->")
         dice_value = random.randint(1,100)
         self.read_Armor(dice_value)
 
 
-    def read_Armor(self, dice_value:int, rarity_category:str='none-cotegory'):
+    def read_Armor(self, dice_value:int, rarity_category:str):
         with open("armors.json", "r", encoding="utf-8") as f:
             armors_data = json.load(f)
         if 1<= dice_value <= 50 :
             self.rarity = "Common"
+            print()
+            sleep(0.5)
             print(f"Vous avez obtenu une armure de rareté : {self.rarity}")
             rarity_category = [item for item in armors_data if item['rarity'] == 'Common']
             
-        if 51<= dice_value <= 80:
+        elif 51<= dice_value <= 80:
             self.rarity = "Rare"
+            print()
+            sleep(0.5)
             print(f"Vous avez obtenu une armure de rareté : {self.rarity}")
             rarity_category = [item for item in armors_data if item['rarity'] == 'Rare']
             
-        if 81<= dice_value <= 95:
+        elif 81<= dice_value <= 95:
             self.rarity = "Epic"
+            print()
+            sleep(0.5)
             print(f"Vous avez obtenu une armure de rareté : {self.rarity}")
             rarity_category = [item for item in armors_data if item['rarity'] == 'Epic']
            
-        if 96<= dice_value <= 100:
+        elif 96<= dice_value <= 100:
             self.rarity = "Legendary"
+            print()
+            sleep(0.5)
             print(f"Vous avez obtenu une armure de rareté : {self.rarity}")
             rarity_category = [item for item in armors_data if item['rarity'] == 'Legendary']
-
-        self.chose_in_rarity(rarity_category)
         
-    def chose_in_rarity(self, rarity_category:str):
-        display_name = [item['name'] for item in rarity_category]
-        print(display_name)
+        return rarity_category
+
         
-    
+    def choice_in_rarity(self, rarity_category:str, choice:int=0):
+        count = 0
+        count2 = 0
+        increment = 0
+        display_names = [item['name'] for item in rarity_category]
+        defense_names = [item['defense'] for item in rarity_category]
+        for name in display_names:
+            count += 1
+        sleep(3)
+        os.system('cls')
+        print()
+        print(f'Vous pouvez choisir {count} armures dans la rareté {self.rarity} :')
+        print()
+        for name in display_names:
+            count2 +=1
+            print()
+            print(f'{name} a {defense_names[increment]} de défense. ({count2})')
+            increment +=1
+        print()
+        self.choice = input("Votre choix : ")
+        self.add_defense_hero(choice)
+
+    def add_defense_hero(self):
+        return
 
 
+        
+        
 Test = Armor()
 Test.dice_roll()
