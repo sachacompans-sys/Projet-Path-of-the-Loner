82122d8569200538e1116c1be9b19b0df19e0dc3
MatysDC <RazoRmatys@gmail.com>
2025-12-06 22:35:06 +0100

    Finalisation des rooms (Room disctinct par niveau avec affichage associé)

--- PvE/room.py
+++ PvE/room.py
@@ -2,41 +2,41 @@ import json
 import random
 from monsters import Monster
 
-
 class Room:
-    def __init__(self, difficulty : int = 0, monsters : int = []):
-        self.difficulty = difficulty
-        self.monsters = monsters
-
+    def __init__(self):
+        self.normals = []
+        self.elites = []
+        self.bosses = []
+        self.load_monsters()
 
     def load_monsters(self):
-        with open('PvE/bestiary.json', 'r', encoding='utf-8') as file:
-            list_data = json.load(file)
+        with open('bestiary.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
 
-        for monster_dict in list_data:
-            new_monster = Monster(monster_dict)
-            self.monsters.append(new_monster)
+        for monster_dict in data:
+            mob = Monster(monster_dict)
+            
+            if mob.type == "Normal":
+                self.normals.append(mob)
+            elif mob.type == "Elite":
+                self.elites.append(mob)
+            elif mob.type == "Boss":
+                self.bosses.append(mob)
 
+    def get_normal_room(self):
+        print("--- Vous entrez dans une salle sombre et humide... ---")
+        mob = random.choice(self.normals)
+        print(f" {mob.name} | (HP: {mob.hp} | Dégâts: {mob.damage} | Défense: {mob.defense}) surgit de l'ombre pour attaquer !")
+        return mob
 
-    def generator_difficulty(self):
-        Normals = []
-        Elites = []
-        Boss = []
+    def get_elite_room(self):
+        print("--- L'air devient lourd, une menace puissante approche... ---")
+        mob = random.choice(self.elites)
+        print(f"ATTENTION : {mob.name} | (HP: {mob.hp} | Dégâts: {mob.damage} | Défense: {mob.defense}) vous barre la route !")
+        return mob
 
-        for i in self.monsters:
-            if i.type == "Normal":
-                Normals.append(i)
-            elif i.type == "Elite":
-                Elites.append(i)    
-            elif i.type == "Boss":
-                Boss.append(i)
-
-        monster_a = random.choice(Normals)
-        monster_b = random.choice(Elites)
-        monster_c = random.choice(Boss)
-
-        return {
-        "Salle A": monster_a,
-        "Salle B": monster_b,
-        "Salle C": monster_c
-        }
+    def get_boss_room(self):
+        print("--- VOUS SENTEZ UNE PRÉSENCE TERRIFIANTE... ---")
+        mob = random.choice(self.bosses)
+        print(f"DANGER MORTEL : {mob.name} | (HP: {mob.hp} | Dégâts: {mob.damage} | Défense: {mob.defense}) est devant vous !!")
+        return mob
